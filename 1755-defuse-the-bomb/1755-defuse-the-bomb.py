066ba6a92f3c6e8b622f864dd3d5d763342d70ad
Yosef A. <125744573+yosefdevx@users.noreply.github.com>
2026-06-02 08:44:22 +0300

    1755-DEFUSE-THE-BOMB Stats: Time: 0 ms (100%), Space: 19.3 MB (75.69%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -4,13 +4,18 @@ class Solution:
         ans = [0]*n
         if k == 0:
             return ans
-        for i in range(n):
-            if k > 0:
-                for j in range(i + 1, i + k + 1):
-                    ans[i] += code[j % n]
-            else:
-                for j in range(i - abs(k), i):
-                    ans[i] += code[(j + n) % n]
+        if k > 0:
+            curr_sum = sum(code[1: 1 + k])
+            ans[0] = curr_sum
+            for i in range(1, n):
+                curr_sum += code[(i + k) % n] - code[i]
+                ans[i] = curr_sum
+        if k < 0:
+            curr_sum = sum(code[k:])
+            ans[0] = curr_sum
+            for i in range(1, n):
+                curr_sum += code[i - 1] - code[i - 1 - abs(k)]
+                ans[i] = curr_sum
         return ans
                 
 
